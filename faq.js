c4fb692c79b01ea9f0ae161f92a47a9168e570a1
villem <>
2022-12-03 17:32:18 +0300

    03.12.22

--- faq.js
+++ faq.js
@@ -48,3 +48,127 @@ function randomRange(min, max) {
 }
 
 // console.log(randomRange(5, 15));
+
+const debounce = (fn, ms) => {
+  let timeoutId = null;
+  return (...args) => {
+    // if (timeoutId) return;
+
+    const process = () => {
+      fn.apply(this, args);
+      clearTimeout(timeoutId);
+      timeoutId = null;
+    };
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(process, ms);
+  };
+};
+
+// function debounce(f, ms) {
+//   let isCooldown = false;
+
+//   return function () {
+//     if (isCooldown) return;
+
+//     f.apply(this, arguments);
+
+//     isCooldown = true;
+
+//     setTimeout(() => (isCooldown = false), ms);
+//   };
+// }
+
+const user = {
+  firstName: 'bob',
+};
+function fetch(url) {
+  console.log(`fetching ${url}...`, this.firstName);
+}
+
+// const f = debounce(fetch.bind(user), 300);
+// f(1);
+// f(2);
+// f(3);
+// f(4);
+// f(5);
+
+const tree = {
+  value: 1,
+  children: [
+    {
+      value: 2,
+      children: [{ value: 3 }],
+    },
+    {
+      value: 4,
+      children: [{ value: 5 }, { value: 6 }],
+    },
+  ],
+};
+
+function flatten(root) {
+  let queue = [root];
+  let result = [];
+
+  while (queue.length) {
+    let node = queue.shift();
+    result.push(node.value);
+    if (node.children) {
+      queue.push(...node.children);
+    }
+  }
+
+  return result;
+}
+
+function flattenR(root) {
+  let result = [];
+
+  function process(node) {
+    result.push(node.value);
+    if (Array.isArray(node.children)) {
+      for (let c of node.children) {
+        process(c, result);
+      }
+    }
+  }
+
+  process(root);
+  return result;
+}
+
+console.log(flattenR(tree));
+
+function p1(a) {
+  return new Promise((resolve, reject) => {
+    resolve(a * 5);
+  });
+}
+
+// promise function 2
+function p2(a) {
+  return new Promise((resolve, reject) => {
+    resolve(a * 2);
+  });
+}
+
+// function 3 - will be wrapped in a resolved promise by .then()
+function f3(a) {
+  return a * 3;
+}
+
+// promise function 4
+function p4(a) {
+  return new Promise((resolve, reject) => {
+    resolve(a * 4);
+  });
+}
+
+function runPromiseInSequence(arr = [], input) {
+  return arr.reduce((promise, fn) => {
+    return promise.then(fn);
+  }, Promise.resolve(input));
+}
+
+const promiseArr = [p1, p2, f3, p4];
+runPromiseInSequence(promiseArr, 10).then(console.log); // 1200
